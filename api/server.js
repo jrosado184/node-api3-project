f058784dd40672578854c253856b4d1693efb4b1
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-12 17:34:10 -0500

    logger middleware built

--- api/server.js
+++ api/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
-
+const userRouter = require("./users/users-router");
+const {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+} = require("./middleware/middleware");
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
@@ -7,8 +13,18 @@ server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
 
+server.use(userRouter);
+
+server.use(logger);
+
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: "something went wrong in the router" && err.message,
+  });
 });
 
 module.exports = server;
