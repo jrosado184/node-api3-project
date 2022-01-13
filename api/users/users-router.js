6030b4f57502615a61cf5d600f7e9f55a2f41b94
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-12 19:12:23 -0500

    post middleware tested and working

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const usersModels = require("./users-model");
 const postsModels = require("../posts/posts-model");
-const { validateUserId, validateUser } = require("../middleware/middleware");
+const {
+  validateUserId,
+  validateUser,
+  validatePost,
+} = require("../middleware/middleware");
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
@@ -42,10 +46,11 @@ router.get("/:id/posts", validateUserId, (req, res) => {
   // this needs a middleware to verify user id
 });
 
-router.post("/:id/posts", validateUserId, (req, res) => {
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.text);
 });
 
 // do not forget to export the router
